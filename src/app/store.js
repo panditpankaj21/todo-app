4d38fdee2b818c83ecf6e0b9d54789afefee14a8
pankaj <pkp20022@gmail.com>
2024-03-30 09:34:49 +0530

    Initialized local storage successfully

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../features/todoSlice.js"
+import { loadState, saveState } from './localStorageUtils';
+
+const preloadedState = loadState();
 
 export const store = configureStore({
-    reducer: todoReducer
+    reducer: todoReducer,
+    preloadedState: preloadedState
 })
+
+store.subscribe(() => {
+    saveState(store.getState());
+});
+  
